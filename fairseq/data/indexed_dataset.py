fbc7d4e84cedc666ddd5fc21a24a59fadfc85eea
Renqian Luo <lrqrichard@gmail.com>
2018-11-22 03:29:22 +0800

    indexed_dataset sizes

--- fairseq/data/indexed_dataset.py
+++ fairseq/data/indexed_dataset.py
@@ -98,7 +98,7 @@ class IndexedDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, i):
         self.check_index(i)
-        tensor_size = self.sizes[self.dim_offsets[i]:self.dim_offsets[i + 1]]
+        tensor_size = self.sizes[i] #self.sizes[self.dim_offsets[i]:self.dim_offsets[i + 1]]
         a = np.empty(tensor_size, dtype=self.dtype)
         self.data_file.seek(self.data_offsets[i] * self.element_size)
         self.data_file.readinto(a)
@@ -135,13 +135,13 @@ class IndexedCachedDataset(IndexedDataset):
         indices = sorted(set(indices))
         total_size = 0
         for i in indices:
-            total_size += self.data_offsets[i + 1] - self.data_offsets[i]
+            total_size += self.sizes[i] #self.data_offsets[i + 1] - self.data_offsets[i]
         self.cache = np.empty(total_size, dtype=self.dtype)
         ptx = 0
         self.cache_index.clear()
         for i in indices:
             self.cache_index[i] = ptx
-            size = self.data_offsets[i + 1] - self.data_offsets[i]
+            size = self.sizes[i] #self.data_offsets[i + 1] - self.data_offsets[i]
             a = self.cache[ptx : ptx + size]
             self.data_file.seek(self.data_offsets[i] * self.element_size)
             self.data_file.readinto(a)
@@ -149,7 +149,7 @@ class IndexedCachedDataset(IndexedDataset):
 
     def __getitem__(self, i):
         self.check_index(i)
-        tensor_size = self.sizes[self.dim_offsets[i]:self.dim_offsets[i + 1]]
+        tensor_size = self.sizes[i] #self.sizes[self.dim_offsets[i]:self.dim_offsets[i + 1]]
         a = np.empty(tensor_size, dtype=self.dtype)
         ptx = self.cache_index[i]
         np.copyto(a, self.cache[ptx : ptx + a.size])
